85a697fe6eaa9397cbdd4cfdb45f1db10714a2f1
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-04-15 17:58:45 -0300

    2. Funcion generateBlock, se instacio y se llamo dentro de la señal

--- scenes/Buttons/VioletGenerator/VioletGenerator.gd
+++ scenes/Buttons/VioletGenerator/VioletGenerator.gd
@@ -1,16 +1,13 @@
 extends TextureButton
 
+var violetgenerator = preload("res://scenes/Blocks/Violet/BlockViolet.tscn")
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func generate_block():
+	var violet = violetgenerator.instance()
+	add_child(violet)
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_VioletGenerator_button_down():
+	generate_block()
+	pass 
