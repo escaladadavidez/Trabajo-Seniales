11a3619f1bd02bb0882b6b69137614e5e68c77cb
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-04-16 00:44:56 -0300

    7. conexion manual de generadores al menu

--- scenes/Buttons/RedGenerator/RedGenerator.gd
+++ scenes/Buttons/RedGenerator/RedGenerator.gd
@@ -2,7 +2,6 @@ extends TextureButton
 
 var redgenerator = preload("res://scenes/Blocks/Red/BlockRed.tscn")
 
-
 func generate_block():
 	var red = redgenerator.instance()
 	add_child(red)
--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -3,14 +3,17 @@ extends Node2D
 const INSTANCES_TEXT = "Cantidad de instancias: "
 # Variable para contar cantidad de instancias
 var instancesCount = 0
-
+signal button_down
 func _ready():
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	$RedGenerator.connect("button_down", self, "count_new_instance")
+	$VioletGenerator.connect("button_down", self, "count_new_instance")	
 	pass
 	
 func count_new_instance():
 	instancesCount += 1
-	$CountLabel.text = "Cantidad de istancias " + String(instancesCount)
-	pass 
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
+
