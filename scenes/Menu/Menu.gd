5d386d06c7ae25cf1a9a4f5738c3a57af68a45d9
David Escalada <62260313+diokai29@users.noreply.github.com>
2020-04-16 00:46:55 -0300

    8. funcion delete creada en la escena menu

--- scenes/Menu/Menu.gd
+++ scenes/Menu/Menu.gd
@@ -16,4 +16,9 @@ func count_new_instance():
 	instancesCount += 1
 	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
 	pass
+	
+func delete():
+	instancesCount = 0
+	$CountLabel.text = INSTANCES_TEXT + String(instancesCount)
+	pass
 
